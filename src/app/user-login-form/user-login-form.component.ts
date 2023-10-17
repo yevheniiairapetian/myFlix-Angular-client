abf15957a795b9e2b9f05b3ab6ec93446e10cb80
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-10-17 16:32:31 +0200

    feat: add login and signup modals, add material design

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls we created in 6.2
+import { UserRegistrationService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() loginData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.loginData).subscribe((result) => {
+
+      localStorage.setItem('user', JSON.stringify(result.user));
+      localStorage.setItem('token', result.token);
+
+      this.dialogRef.close();
+      this.router.navigate(['movies']);
+      this.snackBar.open('Successfully logged in', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open('Please check your credentials', 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+}
